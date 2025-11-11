de6a119acffd9c95d6d4222a7778c9ea55081318
AbSaq <abdulrahman.salem.akeel@gmail.com>
2025-11-11 20:43:38 +0300

    FEAT: created base structure

---
+++ src/main/java/dev/akeel/Main.java
@@ -0,0 +1,8 @@
+package dev.akeel;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Main file!");
+    }
+}
---
+++ src/main/java/dev/akeel/des/constants.java
@@ -0,0 +1,4 @@
+package dev.akeel.des;
+
+public class constants {
+}
